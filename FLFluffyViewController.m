d1babc8dcd5c0fd209707abe34d5a1649bcd3760
Mike Fullerton <mike@greentongue.com>
2013-08-10 20:07:58 -0700

    move up one

---
+++ FLFluffyViewController.m
@@ -0,0 +1,20 @@
+//
+//  FLFluffyViewController.m
+//  FishLampFrameworks
+//
+//  Created by Mike Fullerton on 8/22/12.
+//  Copyright (c) 2013 GreenTongue Software LLC, Mike Fullerton.. 
+//  The FishLamp Framework is released under the MIT License: http://fishlamp.com/license 
+//
+
+#import "FLFluffyViewController.h"
+
+@interface FLFluffyViewController ()
+
+@end
+
+@implementation FLFluffyViewController
+
+
+
+@end
